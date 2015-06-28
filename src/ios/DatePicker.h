f2c258f90628d18f4a0b53b196ea4a2134e73bdd
aabluedragon <aabluedragon@gmail.com>
2015-06-28 17:42:45 +0300

    Update DatePicker.h

--- src/ios/DatePicker.h
+++ src/ios/DatePicker.h
@@ -1,11 +1,11 @@
 /*
  Phonegap DatePicker Plugin for using Cordova 3 and iOS 7
  https://github.com/sectore/phonegap3-ios-datepicker-plugin
- 
+
  Based on a previous plugin version by Greg Allen and Sam de Freyssinet.
- 
+
  Rewrite by Jens Krause (www.websector.de)
- 
+
  MIT Licensed
 */
 
@@ -13,9 +13,10 @@
 #import <Cordova/CDV.h>
 
 @interface DatePicker : CDVPlugin <UIPopoverControllerDelegate> {
-    
+
 }
 
 - (void)show:(CDVInvokedUrlCommand*)command;
+- (void)dismiss:(CDVInvokedUrlCommand*)command;
 
 @end
